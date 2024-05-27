783a5a577896c7512b79335deab79361f77024e2
basiaszafraniec <basia.szafraniec@gmail.com>
2024-05-27 10:08:02 +0200

    1

--- app.js
+++ app.js
@@ -3,6 +3,29 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const teachers = await fetchTeachers();
+  console.log(teachers);
+}
+
+async function fetchTeachers() {
+  const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json ");
+  const data = await response.json();
+  displayTeachers(data);
+  return (data);
+}
+
+function displayTeachers(teachers){
+  const teachersList = document.querySelector("#teachers-list");
+  for (const teacher of teachers){
+    teachersList.insertAdjacentHTML(
+      "beforeend",`
+      <li>
+      ${teacher.name} - ${teacher.title}
+      </li>
+      `
+    )
+    console.log(teacher);
+  }
 }
