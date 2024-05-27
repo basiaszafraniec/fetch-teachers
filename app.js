0393afa1b7906939dc06836b18dbb7981b0b6c51
basiaszafraniec <basia.szafraniec@gmail.com>
2024-05-27 10:39:32 +0200

    done

--- app.js
+++ app.js
@@ -6,26 +6,42 @@ window.addEventListener("load", initApp); // When the page is loaded, run initAp
 async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
   const teachers = await fetchTeachers();
-  console.log(teachers);
+  displayTeachersGrid(teachers);
+  // console.log(teachers);
 }
 
 async function fetchTeachers() {
   const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json ");
   const data = await response.json();
-  displayTeachers(data);
   return (data);
 }
 
-function displayTeachers(teachers){
+function displayTeachers(teachers) {
   const teachersList = document.querySelector("#teachers-list");
-  for (const teacher of teachers){
+  for (const teacher of teachers) {
     teachersList.insertAdjacentHTML(
-      "beforeend",`
+      "beforeend", `
       <li>
       ${teacher.name} - ${teacher.title}
       </li>
       `
     )
     console.log(teacher);
+  }
+}
+
+function displayTeachersGrid(teachers) {
+  const teachersGrid = document.querySelector("#teachers-grid");
+  for (const teacher of teachers) {
+    teachersGrid.insertAdjacentHTML(
+      "beforeend", `
+      <article class="grid-item">
+      <img src="${teacher.image}" alt="${teacher.name}" />
+      <h2>${teacher.name}</h2>
+      <p>${teacher.title}</p>
+      <a href="#"> ${teacher.mail}</a>
+    </article>
+      `
+    )
   }
 }
